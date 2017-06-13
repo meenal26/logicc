ed3a0da79a1c29df4260db532c7bc6739132db68
meenal26 <meenalmeenu26@gmail.com>
2017-06-13 10:05:40 +0530

    Add files via upload

---
+++ oddbw2.java
@@ -0,0 +1,16 @@
+package guvi;
+
+public class oddbw2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=0;i<=10;i++){
+			if(i%2!=0){
+				System.out.println(+i);
+			}
+		}
+
+	}
+
+}
