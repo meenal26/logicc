2ba33088cef5a9a05d97847e67119862d30bb8e9
meenal26 <meenalmeenu26@gmail.com>
2017-06-13 10:01:30 +0530

    Add files via upload

---
+++ evenbw2.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class evenbw2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=0;i<=10;i++){
+			if(i%2==0){
+				System.out.println(+i);
+			}
+		}
+	}
+
+}
